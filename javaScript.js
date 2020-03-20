0e912a157874fd736acf112a90edff225ea8f669
Romiljain258 <romiljain258@gmail.com>
2020-03-20 15:23:06 +0530

    first commit

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+function tipCalculator(){
+    var fee=document.getElementById("fee").value;
+    var percentage = document.getElementById("percentage").value;
+  
+    alert( fee * percentage / 100);
+}
